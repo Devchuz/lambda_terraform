c130238ac33ff3c011fe0beb23d09428e79f263b
Devchuz <jhon.rincon.r@uni.pe>
2024-10-19 20:43:16 -0500

    feat: delete version terraform

--- variables.tf
+++ variables.tf
@@ -2,8 +2,3 @@ variable "service_names" {
   description = "A map of service names to deploy"
   type        = map(string)
 }
-
-variable "region" {
-  description = "Region where AWS resources will be deployed"
-  default     = "us-east-1"
-}
